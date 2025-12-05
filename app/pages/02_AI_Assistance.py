41ac5a7fe15ff0ce5fc3db78cc24440cd4ea79ea
NadineJac <nadine.jacobsen@outlook.de>
2025-12-05 08:50:04 +0100

    now with correct names

--- app/pages/02_AI_Assistance.py
+++ app/pages/02_AI_Assistance.py
@@ -68,10 +68,10 @@ if st.session_state.get("physhealth_value"):
     user_profile["physical_health_poor"] = (f"{st.session_state["physhealth_value"]} days/month")
 if st.session_state.get("risk_value"):
     user_profile["risk"] = (f"{st.session_state["risk_value"]}%")
-if st.session_state.get("weight"):
-    user_profile["weight"] = st.session_state["weight"]
-if st.session_state.get("height"):
-    user_profile["height"] = st.session_state["height"]
+if st.session_state.get("weight_value"):
+    user_profile["weight_value"] = st.session_state["weight_value"]
+if st.session_state.get("height_value"):
+    user_profile["height_value"] = st.session_state["height_value"]
 
 
 profile_text = ""
